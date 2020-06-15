e3fbfcaa867e2561f7dbb49c233a6de890e7aae7
Juhee Baek <juhee.baek@fitogether.com>
2020-06-15 13:50:30 +0900

    #9 Creating a Block part Two

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,20 @@
+import * as CryptoJs from "crypto-js";
+
 class Block {
   public index: number;
   public hash: string;
   public previousHash: string;
   public data: string;
   public timestamp: number;
+
+  static calculateBlockHash = (
+    index: number,
+    previousHash: string,
+    timestamp: number,
+    data: string
+  ): string =>
+    CryptoJs.SHA256(index + previousHash + timestamp + data).toString();
+
   constructor(
     index: number,
     hash: string,
@@ -19,10 +30,19 @@ class Block {
   }
 }
 
+// calculateBlockHash 앞에 static이 없으면 쓸 수 없음.
+// Block.calculateBlockHash()
+
 // argument에 gender? 이렇게 쓰면 저건 필수가 아닌 선택을 의미한다.
 const genesisBlock: Block = new Block(0, "2020202020202", "", "Hello", 123456);
 
-let blockchain: [Block] = [genesisBlock];
+let blockchain: Block[] = [genesisBlock];
+
+const getBlockChain = (): Block[] => blockchain;
+
+const getLastBlock = (): Block => blockchain[blockchain.length - 1];
+
+const getNewTimeStamp = (): number => Math.round(new Date().getTime() / 1000);
 
 console.log(blockchain);
 
