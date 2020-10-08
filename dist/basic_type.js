c7c39849960b8d5ed90263cd5c35c898fa79eebe
Juhee <juhee.baek@fitogether.com>
2020-10-08 18:01:08 +0900

    interface

---
+++ dist/basic_type.js
@@ -0,0 +1,41 @@
+let numValue;
+let stringValue;
+let booleanValue;
+let undefinedValue;
+let nullValue;
+let objValue;
+let symbolValue;
+let anyValue;
+numValue = 3.3;
+stringValue = "aaa";
+stringValue = 'hello';
+stringValue = `
+hello
+${1 + 1}
+hi
+`;
+booleanValue = true;
+undefinedValue = null;
+numValue = null;
+numValue = undefined;
+anyValue = 1;
+anyValue = "3";
+anyValue = null;
+anyValue = {};
+objValue = { name: "juhee" };
+objValue = {};
+objValue = new String(33);
+symbolValue = Symbol();
+let nameList;
+nameList = ["1", "2"];
+let user1;
+user1 = {
+    name: "juhee",
+    score: 100
+};
+let user2;
+let tuple2;
+tuple2 = [2, "hello"];
+let tuple3;
+tuple3 = [2, 2, 3];
+//# sourceMappingURL=basic_type.js.map
--- dist/index.js
+++ dist/index.js
@@ -5,7 +5,6 @@ const person = {
     age: 29,
     gender: "female"
 };
-// argument에 gender? 이렇게 쓰면 저건 필수가 아닌 선택을 의미한다.
 const sayHi = (person) => {
     return `Hello ${person.name}, you are ${person.age},  you are a ${person.gender}`;
 };
